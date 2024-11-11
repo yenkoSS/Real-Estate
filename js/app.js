6d91a0deba132eef5496a02ef87c98abafc6e779
Stefan Petru-Cornel <petrucornel.stefan@gmail.com>
2024-11-11 23:08:49 +0200

    add responsive navbar

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
 const rowsList = document.querySelectorAll(".row");
 const headingSectionList = document.querySelectorAll(".heading-section");
+const iconClose = document.querySelector(".icon-close");
+const menuBtn = document.querySelector(".menu-box");
+const menu = document.querySelector(".nav-list-small");
+const navItemList = document.querySelectorAll(".nav-item");
 
 const rowsOberver = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
@@ -18,3 +22,15 @@ const headingObserver = new IntersectionObserver((entries) => {
 
 rowsList.forEach((row) => rowsOberver.observe(row));
 headingSectionList.forEach((heading) => headingObserver.observe(heading));
+
+menuBtn.addEventListener("click", () => {
+  menu.style.display = "flex";
+});
+
+iconClose.addEventListener("click", () => {
+  menu.style.display = "none";
+});
+
+navItemList.forEach((navItem) => {
+  navItem.addEventListener("click", () => (menu.style.display = "none"));
+});
